5864cafa942ec2a5e99a8da2464e5f501e6af73a
wangbin <wangb@nextechat.com>
2017-06-29 15:22:14 +0800

    add size for svg icon

--- Icon.vue
+++ Icon.vue
@@ -1,11 +1,11 @@
 <template>
   <svg version="1.1" :class="clazz" :role="label ? 'img' : 'presentation'" :aria-label="label" :width="width"
        :height="height" :viewBox="box" :style="style">
-    <path :d="path.d" :fill="path.fill" :stroke="path.stroke" v-for="path in icon.paths"/>
+    <path :d="path.d" :fill="path.fill" :stroke="path.stroke" v-for="(path, index) in icon.paths" :key="index"/>
   </svg>
 </template>
 
-<style>
+<style scoped>
   .svg-icon {
     display: inline-block;
     fill: currentColor;
@@ -35,74 +35,87 @@
 
 <script>
 
-  const convert = require('./lib/parse');
+import * as convert from 'lib/parse'
 
-  export default {
-    props: {
-      name: {
-        type: String,
-        required: true
-      },
-      scale: [Number, String],
-      spin: Boolean,
-      flip: {
-        validator: function (val) {
-          return val === 'horizontal' || val === 'vertical'
-        }
-      },
-      label: String,
-      index: String,
-      currentIndex: String
+function normalized (val, def) {
+  val = typeof val === 'undefined' ? 1 : Number(val)
+  if (isNaN(val) || val <= 0) {
+    console.warn(`Invalid: val should be a number over 0.`, this)
+    return def
+  }
+  return val
+}
+
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
     },
-    computed: {
-      normalizedScale() {
-        let scale = this.scale;
-        scale = typeof scale === 'undefined' ? 1 : Number(scale);
-        if (isNaN(scale) || scale <= 0) {
-          console.warn(`Invalid prop: prop "scale" should be a number over 0.`, this);
-          return 1
-        }
-        return scale
-      },
-      clazz() {
-        return {
-          'svg-icon': true,
-          spin: this.spin,
-          'flip-horizontal': this.flip === 'horizontal',
-          'flip-vertical': this.flip === 'vertical',
-          active: this.index === this.currentIndex
-        }
-      },
-      icon() {
-        let xml = require(`!xml-loader!../../src/svg/${this.name}.svg`);
-        const t = xml.svg.$.viewBox.split(' ');
-        console.info(`src/svg/${this.name}.svg has been loaded`);
-        return {
-          width: t[2],
-          height: t[3],
-          paths: convert.SVGtoArray(xml.svg)
-        }
-      },
-      box() {
-        return `0 0 ${this.icon.width} ${this.icon.height}`
-      },
-      width() {
-        return this.icon.width / 112 * this.normalizedScale
-      },
-      height() {
-        return this.icon.height / 112 * this.normalizedScale
-      },
-      style() {
-        if (this.normalizedScale === 1) {
-          return false
-        }
-        return {
-          fontSize: this.normalizedScale + 'em'
-        }
+    scale: [Number, String],
+    size: [Number, String],
+    spin: Boolean,
+    flip: {
+      validator: function (val) {
+        return val === 'horizontal' || val === 'vertical'
       }
     },
-    register: function () {
-      console.warn("inject deprecated since v1.2.0, SVG files can be loaded directly, so just delete the inject line.")
+    label: String,
+    index: String,
+    currentIndex: String
+  },
+  computed: {
+    normalizedScale () {
+      return normalized(this.scale, 1)
     },
+    normalizedSize () {
+      return normalized(this.size, 0)
+    },
+    clazz () {
+      return {
+        'svg-icon': true,
+        spin: this.spin,
+        'flip-horizontal': this.flip === 'horizontal',
+        'flip-vertical': this.flip === 'vertical',
+        active: this.index === this.currentIndex
+      }
+    },
+    icon () {
+      let xml = require(`!xml-loader!svg/${this.name}.svg`)
+      const t = xml.svg.$.viewBox.split(' ')
+      console.info(`src/svg/${this.name}.svg has been loaded`)
+      return {
+        width: t[2],
+        height: t[3],
+        paths: convert.SVGtoArray(xml.svg)
+      }
+    },
+    box () {
+      return `0 0 ${this.icon.width} ${this.icon.height}`
+    },
+    width () {
+      if (this.normalizedSize) {
+        return this.normalizedSize * this.normalizedScale
+      }
+      return this.icon.width / 112 * this.normalizedScale
+    },
+    height () {
+      if (this.normalizedSize) {
+        return this.normalizedSize * this.icon.height / this.icon.width * this.normalizedScale
+      }
+      return this.icon.height / 112 * this.normalizedScale
+    },
+    style () {
+      if (this.normalizedScale === 1) {
+        return false
+      }
+      return {
+        fontSize: this.normalizedScale + 'em'
+      }
+    }
+  },
+  register: function () {
+    console.warn('inject deprecated since v1.2.0, SVG files can be loaded directly, so just delete the inject line.')
   }
+}
 </script>
